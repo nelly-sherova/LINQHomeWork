57cf962566a11cc2b429650164c2a26cdd92ff84
unknown <nellysherova@mail.ru>
2021-03-15 16:40:08 +0300

    Добавила решение 2.2 задачи

--- LINQHW/Program.cs
+++ LINQHW/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace LINQHW
 {
@@ -18,6 +19,19 @@ namespace LINQHW
             foreach (var item in answer)
                 Console.Write($"{item};  ");
             Console.WriteLine("}");
+
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Task 2.2 :  ");
+            Console.ResetColor();
+           
+            Console.WriteLine("Enter array elements: ");
+            string[] numbers = Console.ReadLine().Split();
+
+            var positiveCount = numbers.Where(n => int.Parse(n) > 0).Count();
+            var negativeSum = numbers.Where(n => (int.Parse(n) < 0)).Sum(n => int.Parse(n));
+           
+            Console.WriteLine($"[{positiveCount},  {negativeSum}]");
         }
     }
 }
