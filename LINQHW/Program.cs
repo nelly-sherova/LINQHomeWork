84320ebb726c3bfbfba9ad9803323bbcc08dc54e
unknown <nellysherova@mail.ru>
2021-03-15 16:06:37 +0300

    Начало работы

---
+++ LINQHW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
