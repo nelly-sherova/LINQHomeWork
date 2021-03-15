f6cd2d64deb5cd275c2844550ad92444a13b3fa8
unknown <nellysherova@mail.ru>
2021-03-15 16:19:02 +0300

    Добавила решение 2.1 Задания

--- LINQHW/Program.cs
+++ LINQHW/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQHW
 {
@@ -6,7 +7,17 @@ namespace LINQHW
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Task 2.1 :  ");
+            Console.ResetColor();
+            Console.Write("Enter number: "); int number = Convert.ToInt32(Console.ReadLine());
+            var answer = number.ToString().ToCharArray().Reverse();
+
+            Console.WriteLine($"Your Number: {number}");
+            Console.Write("Result: {");
+            foreach (var item in answer)
+                Console.Write($"{item};  ");
+            Console.WriteLine("}");
         }
     }
 }
